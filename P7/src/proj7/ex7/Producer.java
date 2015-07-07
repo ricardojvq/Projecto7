9182e0295e67062ec7a03f08df75a2022a97dbb4
Ricardo Quirino <ricardojvquirino@gmail.com>
2015-07-07 21:40:41 +0100

    ex7

--- P7/src/proj7/ex7/Producer.java
+++ P7/src/proj7/ex7/Producer.java
@@ -1,6 +1,8 @@
 package proj7.ex7;
 
 import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 /**
  * Created by ricardoquirino on 07/07/15.
@@ -12,10 +14,37 @@ public class Producer implements Runnable {
 
     @Override
     public void run() {
-        while (true) {
-            for (int i = 1; i < 10; i++) {
-                System.out.println(Thread.currentThread().getId() + " a correr pela "+i+"ª vez!");
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            for (int j = 0; j < 2; j++) {
+                System.out.println("Producing cookies..." + " (" + Thread.currentThread().getId() + ")");
+                Random rd = new Random();
+                int min = 1;
+                int max = 100000000;
+                Integer r = min + rd.nextInt(max - min + 1);
+                try {
+                    Main.lista.put(r);
+                } catch (InterruptedException e) {
+                    System.out.println("Cookies' production stoped...");
+                }
+            }
+
+            System.out.println("Waiting for other Cookies' producers...");
+
+            try {
+                Main.barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("No more cookies... Restarting production!");
         }
+
     }
 }
